e9ed7ac7dd4e27dd584b37a5411039ea1ebfb4ef
James Barnard <jmbarnard@gmail.com>
2025-02-06 15:09:29 +0000

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,51 @@
+const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
+const markdownIt = require("markdown-it");
+const esbuild = require("esbuild");
+const { NODE_ENV = 'production' } = process.env
+const isProduction = NODE_ENV === 'production'
+
+module.exports = function (config) {
+
+  config.on('eleventy.before', async () => {
+    await esbuild.build({
+      entryPoints: ['src/assets/scripts/main.js'],
+      bundle: true,
+      sourcemap: true,
+      outfile: 'public/assets/scripts/main.js',
+      target: isProduction? 'es6' : 'esnext'
+    });
+  });
+
+  const md = new markdownIt({
+    html: true
+  });
+
+  config.addPairedShortcode("markdown", (content) => {
+    return md.render(content);
+  });
+
+  config.addWatchTarget("./src/assets/styles/");
+  config.addShortcode("year", () => {
+    return `${new Date().getFullYear()}`
+  });
+  config.addWatchTarget('./src/assets/scripts/');
+  config.addPassthroughCopy("./src/assets/fonts/");
+  config.addPassthroughCopy("./src/assets/images/");
+  config.addPassthroughCopy("./src/assets/*.json");
+  config.addPassthroughCopy("./tls/");
+  config.addPlugin(eleventySass);
+  // config.setServerOptions({
+  //   https: {
+  //     key: "tls/localhost-key.pem",
+  //     cert: "tls/localhost-cert.pem"
+  //   }
+  // });
+
+  return {
+    templateFormats: ['md', 'njk'],
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};
---
+++ src/assets/scripts/main.js
@@ -0,0 +1,8 @@
+
+import nav from './nav';
+import contact from './contact';
+import animate from './animations';
+
+nav();
+contact();
+animate();
